800e1667d5770c074e5736b9e359d93fd23568e1
vishishta04 <vishishtaiit@gmail.com>
2023-12-06 14:01:24 +0530

    new flassk update

--- flask_model.py
+++ flask_model.py
@@ -14,9 +14,6 @@ def home():
     return render_template('index.html')
 
 
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
