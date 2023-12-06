c3cdf56f7910c13d39aa94c1e9cfe42470590885
vishishta04 <vishishtaiit@gmail.com>
2023-12-06 13:50:45 +0530

    model updates

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
+#divide into training and testing
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
